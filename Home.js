e0f3a40620120c163ddb4f2138534eb9952c2687
Teodoris0 <129110964+Teodoris0@users.noreply.github.com>
2023-03-27 21:20:48 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Begginer at Programming", "Noob at Programming?", "BOBLOC player?", "FORTNITE KIDDO?", "nah im joking im not a", "FORTNITE KIDDO"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+})
